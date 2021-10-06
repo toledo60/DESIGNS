3e02b9d5fe71c9394b91eff0a03ef38ca93bcce1
Jose Toledo <toledojose60@gmail.com>
2021-10-05 20:38:31 -0700

    added conference designs from the paper "Projections of Definitive Screening Designs by Dropping Columns: Selection and Evaluation" k=2-12

---
+++ data-raw/conference_designs.R
@@ -0,0 +1,17 @@
+conf2 = as.matrix(read.table("data-raw/Conference/c2.txt",
+                              quote="\"", comment.char=""))
+
+conf4 = as.matrix(read.table("data-raw/Conference/c4.txt",
+                              quote="\"", comment.char=""))
+
+conf6 = as.matrix(read.table("data-raw/Conference/c6.txt",
+                              quote="\"", comment.char=""))
+
+conf8 = as.matrix(read.table("data-raw/Conference/c8.txt",
+                               quote="\"", comment.char=""))
+
+conf10 = as.matrix(read.table("data-raw/Conference/c10.txt",
+                               quote="\"", comment.char=""))
+
+conf12 = as.matrix(read.table("data-raw/Conference/c12.txt",
+                               quote="\"", comment.char=""))
